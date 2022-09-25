86d4571eec31404a06df72a99de97fc0b6fa5f9d
akrdesign <akrdesign04@gmail.com>
2022-09-25 16:43:21 +0530

    implementing preloading transitions

--- app/index.js
+++ app/index.js
@@ -1,15 +1,23 @@
 import  each  from 'lodash/each'
 
+import Preloader from 'components/Preloader'
+
 import About from 'pages/About'
 import Collections from 'pages/Collections'
 import Detail from 'pages/Detail'
 import Home from 'pages/Home'
 class App {
   constructor () {
+    this.createPreloader()
     this.createContent()
     this.createPages()
 
     this.addEventListeners()
+  }
+
+  createPreloader () {
+    this.preloader = new Preloader()
+    this.preloader.once('completed', this.onPreloaded.bind(this))
   }
 
   createContent() {
@@ -27,6 +35,11 @@ class App {
 
     this.page = this.pages[this.template]
     this.page.create()
+  }
+
+  onPreloaded() {
+    this.preloader.destroy()
+
     this.page.show()
   }
 
@@ -50,6 +63,8 @@ class App {
       this.page = this.pages[this.template]
       this.page.create()
       this.page.show()
+
+      this.addEventListeners()
     } else {
       console.log('Error')
     }
